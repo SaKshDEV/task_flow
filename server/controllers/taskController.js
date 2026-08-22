572a1a972f194299e65fc8a7b948b7f05107cc43
saksham000003 <2k23csaiml2314139@gmail.com>
2026-08-22 10:04:14 +0530

    task controller addedd

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,32 @@
 const Task = require("../models/Task");
 
-const create
+const createTask = async(req,res)=>{
+    try{
+        const { title,priority,description} = req.body;
+
+        if(!title){
+            res.status(400).json({
+                message:"title is required"
+        });
+        }
+        const task = await task.create({
+            title,
+            description,
+            priority,
+            user: req.user.userId
+        });
+        res.status(201).json({
+         message: "task created succesfully",
+         task
+        });
+     }catch(error){
+        res.status(500).json({
+            message: "server error",
+            error: error.message
+        });
+     }
+}
+module.exports={
+    createTask
+};
 
