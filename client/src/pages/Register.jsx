64767c9230999302984021407604a776329868b1
saksham000003 <2k23csaiml2314139@gmail.com>
2026-08-24 12:17:39 +0530

    register page done

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,78 @@
-function Register(){
-    return(
-        <div>
-            <h1> Register Page</h1>
-        </div>
-    )
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+import api from "../services/api"
+
+function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+
+const navigate = useNavigate();
+
+const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+        await api.post("/auth/register", {
+            name,
+            email,
+            password
+        });
+
+        navigate("/login");
+
+    } catch (error) {
+
+        setMessage(
+            error.response?.data?.message ||
+            "registration failed"
+        )
+    }
+
 }
 
+return (
+    <div>
+        <h1> Create taskFlow account</h1>
+
+        <form onSubmit={handleRegister}>
+            <input type="text"
+                placeholder="enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)} /><br /> <br />
+            <input
+                type="email"
+                placeholder="enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+
+
+            />
+            <input
+                type="password"
+                placeholder="enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+
+
+            /><br /> <br />
+            <button type="submit">
+                register
+
+            </button>
+
+        </form>
+        {message && <p>{message}</p>}
+
+        <p>
+            Already have an account?{""}
+            <Link to= "/login">
+            login</Link>
+            
+        </p>
+    </div>
+);
+}
 export default Register;
