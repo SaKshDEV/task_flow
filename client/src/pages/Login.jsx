9cf9549a7fe66869917ecb9b0b43f4e357822821
saksham000003 <2k23csaiml2314139@gmail.com>
2026-08-24 10:54:20 +0530

    connect login page with backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,65 @@
-function Login() {
-    return(
-        <div>
-        <h1>Login Page</h1>
-        </div>
-    )
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
+import api from "../services/api";
+
+
+
+function Login() {
+
+const [email,setEmail] = useState("");
+const [password,setPassword] = useState("");
+const [message,setMessage] = useState("");
+
+const navigate = useNavigate();
+
+const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try{
+        const response = await api.post("/auth/login", {
+            email,
+            password
+        });
+        localStorage.setItem(
+            "token",
+            response.data.token
+        )
+        localStorage.setItem(
+            "user",
+            JSON.stringify(response.data.user)
+        );
+        navigate("/dashboard");
+    }catch(error){
+        setMessage(
+            error.response?.data?.message ||
+            "login failed"
+        );
+    };
+};
+return(
+    <div>
+    <h1>Taskflow Login</h1>
+
+    <form onSubmit={handleLogin}>
+        <input type="email" placeholder="enter email" value={email} onChange={(e)=> setEmail(e.target.value)} /><br/><br/>
+
+        <input type="password" placeholder="enter password" value={password} onChange={(e)=> setPassword(e.target.value)} /><br/><br/>
+
+        <button type="submit">Login</button>
+    </form>
+
+    {message && <p>{message}</p>}
+    
+      <p>
+                Don't have an account?{" "}
+                <Link to="/register">
+                    Register
+                </Link>
+            </p>
+
+
+    </div>
+);
 }
 export default Login;
