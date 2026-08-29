c9f1c39f3fa70842f52b41a69b9eba14294613da
saksham000003 <2k23csaiml2314139@gmail.com>
2026-08-29 21:57:31 +0530

    fix cors related backend issue

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const express=require("express");
-const cors= require("cors");
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const taskRoutes = require("./routes/taskRoutes");
 
@@ -7,24 +7,31 @@ const taskRoutes = require("./routes/taskRoutes");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes")
 
- const app = express();
+const app = express();
 
- connectDB();
- 
- app.use(cors());
- app.use(express.json());
+connectDB();
 
- app.use("/api/auth", authRoutes);
- app.use("/api/tasks", taskRoutes);
+app.use(cors({
+   origin: [
+      "http://localhost:5173",
+      "https://task-flow-seven-navy.vercel.app"
+   ],
+   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+   allowedHeaders: ["Content-Type", "Authorization"]
+}));
+app.use(express.json());
 
- app.get("/",(req,res) =>{
-    res.json({
-        message: "task flow api is running"
-    });
- });
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
- const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+   res.json({
+      message: "task flow api is running"
+   });
+});
 
- app.listen( PORT ,()=>{
-    console.log(`sever running on port ${PORT}`);
- });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+   console.log(`sever running on port ${PORT}`);
+});
